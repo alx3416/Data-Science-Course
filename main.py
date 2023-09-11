25b6d2d3bdf04709152c0cb0c268b74c373b1426
alx3416 <63263335+alx3416@users.noreply.github.com>
2023-09-11 17:58:36 -0600

    adding linear regression

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ data = imp.read_diabetes_txt("data/diabetes.tab.txt")
 print("archivo txt cargado")
 
 correlation_data = proc.get_correlations(data)
-visu.save_histograms(data)
+# visu.save_histograms(data)
 
-visu.save_scatter_plots(data, correlation_data)
+# visu.save_scatter_plots(data, correlation_data)
 
-visu.save_histogram_correlations(data)
+# visu.save_histogram_correlations(data)
 
 data_normalized = proc.normalize_diabetes_data(data)
 print("data normalized")
@@ -21,3 +21,15 @@ print("data normalized")
 # x = data["AGE"]
 # x = data.loc[:, "AGE"]
 # x.mean()
+
+input_data = data_normalized["BMI"]
+output_data = data_normalized["Y"]
+training_input, test_input = proc.split_data(input_data)
+training_output, test_output = proc.split_data(output_data)
+
+model = proc.simple_linear_regression(training_input, training_output)
+
+test_predictions = proc.test_predictions(model, test_input)
+
+print(proc.get_coefficients(model))
+
--- utils/processing.py
+++ utils/processing.py
@@ -1,6 +1,9 @@
 import os
 import math as mt
 import pandas as pd
+import numpy as np
+from sklearn import linear_model
+from sklearn.metrics import mean_squared_error, r2_score
 
 
 def check_output_folder(path):
@@ -32,3 +35,37 @@ def normalize_diabetes_data(data):
     x3["Y"] = data["Y"]
     return x3
 
+
+def use_only_one_feature(data):
+    diabetes_one = data[:, np.newaxis, 2]
+    return diabetes_one
+
+
+def split_data(data):
+    training_data = data[:-20]
+    test_data = data[-20:]
+    return training_data, test_data
+
+
+def simple_linear_regression(input_train, output_train):
+    regr = linear_model.LinearRegression()
+    inputs = input_train.values.reshape(-1, 1)
+    regr.fit(inputs, output_train)
+    return regr
+
+
+def test_predictions(model, input_test):
+    output_test = model.predict(input_test.values.reshape(-1, 1))
+    return output_test
+
+
+def get_coefficients(model):
+    return model.coef_
+
+
+def get_mean_squared_error(output_test, output_predicted):
+    return mean_squared_error(output_test, output_predicted)
+
+
+def get_coefficient_determination(output_test, output_predicted):
+    return r2_score(output_test, output_predicted)
